3e9a49af0a1a049bcabeb794ee42b669e26460cc
MatheusCosta7p <matheus.costa72@fatec.sp.gov.br>
2025-03-16 22:46:27 -0300

    Move data.json to public folder for GitHub Pages access

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { IProductsResponse } from "src/types/product.type";
 
-const PRODUCTS_JSON_URL = "/teste-front-end/src/data/data.json";
+const PRODUCTS_JSON_URL = "/teste-front-end/data.json";
 
 
 export const fetchProducts = async () : Promise<IProductsResponse> => {
